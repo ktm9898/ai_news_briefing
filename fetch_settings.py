4ee0d845e2bfffacb8a0fc8c12d92ec83854296d
ktm9898 <ktm9898@gmail.com>
2026-03-12 15:05:18 +0900

    Cleanup: Remove obsolete files and redundant install.html

--- fetch_settings.py
+++
@@ -1,4 +0,0 @@
-import urllib.request, json
-res = urllib.request.urlopen("https://script.google.com/macros/s/AKfycbwtgUJ0QfKHPn6HLt2U41OLUog92RFVaKPyLLVwQodq03LMm50qm4dwQk798D3ovFcaHw/exec?tab=Settings")
-with open("settings_out.json", "w", encoding="utf-8") as f:
-    f.write(res.read().decode("utf-8"))
